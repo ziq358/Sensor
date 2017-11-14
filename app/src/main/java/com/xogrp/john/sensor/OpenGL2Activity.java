514bb2c7a23e83233a4f143d442519d6a0c788f1
jwu <jwu@xogrp.com>
2017-11-14 14:57:20 +0800

    opengl 显示了全景图

--- app/src/main/java/com/xogrp/john/sensor/OpenGL2Activity.java
+++ app/src/main/java/com/xogrp/john/sensor/OpenGL2Activity.java
@@ -1,10 +1,13 @@
 package com.xogrp.john.sensor;
 
-import android.opengl.GLES10;
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.support.v7.app.AppCompatActivity;
+import android.opengl.Matrix;
 import android.os.Bundle;
+
+import com.xogrp.john.sensor.openGl.OpenGlUtil;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -13,62 +16,170 @@ import java.nio.FloatBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-public class OpenGL2Activity extends AppCompatActivity {
+public class OpenGL2Activity extends Activity {
 
+    private static String TAG = "ziq";
     GLSurfaceView glSurfaceView;
+    private String filePath="images/texture_360_n.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_gl2);
-
         glSurfaceView = (GLSurfaceView) findViewById(R.id.glSurfaceView);
-
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(new MyRenderer(OpenGlUtil.loadBitmapFromAssets(getBaseContext(), filePath)));
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
     }
 
     class MyRenderer implements GLSurfaceView.Renderer {
+        protected int surfaceWidth,surfaceHeight;
+        Bitmap bitmap;
 
-        float[] vertices = {
-                0.0f, 0.0f,
-                1.0f, 0.0f,
-                0.5f, 1.0f
+        private int imageTextureId;
+        private int imageSize[];
+
+        private int mProgramId;
+        private String mVertexShader = "attribute vec4 aPosition;\n" +
+                                        "attribute vec4 aTextureCoord;\n" +
+                                        "varying vec2 vTextureCoord;\n" +
+                                        "uniform mat4 uMVPMatrix;\n" +
+                                        "void main() {\n" +
+                                        "  gl_Position = uMVPMatrix * aPosition;\n" +
+                                        "  vTextureCoord = aTextureCoord.xy;\n" +
+                                        "}";
+
+        private String mFragmentShader =    "precision mediump float;\n" +
+                                            "varying vec2 vTextureCoord;\n" +
+                                            "uniform sampler2D sTexture;\n" +
+                                            "void main() {\n" +
+                                            "    gl_FragColor=texture2D(sTexture, vTextureCoord);\n" +
+                                            "}";
+
+        private int maPositionHandle;
+        private int maTextureCoordinateHandle;
+        private int uMVPMatrixHandle;
+        private int uTextureSamplerHandle;
+
+        private FloatBuffer mVerticesBuffer;
+        private FloatBuffer mTexCoordinateBuffer;
+        private final float TRIANGLES_DATA_CW[] = {
+                -1.0f, -1.0f, 0f, //LD
+                -1.0f, 1.0f, 0f,  //LU
+                1.0f, -1.0f, 0f,  //RD
+                1.0f, 1.0f, 0f    //RU
         };
 
-        float[] textureVertices = {
+        public final float TEXTURE_NO_ROTATION[] = {
+                0.0f, 1.0f,
                 0.0f, 0.0f,
-                1.0f, 0.0f,
-                0.5f, 1.0f
+                1.0f, 1.0f,
+                1.0f, 0.0f
         };
 
-        private FloatBuffer verticesBuffer;
-        private FloatBuffer textureBuffer;
+        protected float[] projectionMatrix = new float[16];
+
+
+        public MyRenderer(Bitmap bitmap) {
+            this.bitmap = bitmap;
+            mVerticesBuffer =ByteBuffer.allocateDirect(
+                    TRIANGLES_DATA_CW.length * 4)
+                    .order(ByteOrder.nativeOrder())
+                    .asFloatBuffer()
+                    .put(TRIANGLES_DATA_CW);
+            mVerticesBuffer.position(0);
+
+            mTexCoordinateBuffer =ByteBuffer.allocateDirect(
+                    TEXTURE_NO_ROTATION.length * 4)
+                    .order(ByteOrder.nativeOrder())
+                    .asFloatBuffer()
+                    .put(TEXTURE_NO_ROTATION);
+            mTexCoordinateBuffer.position(0);
+        }
 
         @Override
         public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
 
-            ByteBuffer buff = ByteBuffer.allocateDirect(vertices.length * 4);
-            buff.order(ByteOrder.nativeOrder());
-            verticesBuffer = buff.asFloatBuffer();
-            verticesBuffer.put(vertices);
-            verticesBuffer.position(0);
+            imageTextureId = OpenGlUtil.getTextureFromBitmap(bitmap, imageSize);
 
-            buff = ByteBuffer.allocateDirect(textureVertices.length * 4);
-            buff.order(ByteOrder.nativeOrder());
-            textureBuffer = buff.asFloatBuffer();
-            textureBuffer.put(textureVertices);
-            textureBuffer.position(0);
+            mProgramId = OpenGlUtil.createProgram(mVertexShader, mFragmentShader);
+            if (mProgramId == 0) {
+                return;
+            }
+            maPositionHandle = GLES20.glGetAttribLocation(mProgramId, "aPosition");
+            OpenGlUtil.checkGlError("glGetAttribLocation aPosition");
+            if (maPositionHandle == -1) {
+                throw new RuntimeException("Could not get attrib location for aPosition");
+            }
+            maTextureCoordinateHandle = GLES20.glGetAttribLocation(mProgramId, "aTextureCoord");
+            OpenGlUtil.checkGlError("glGetAttribLocation aTextureCoord");
+            if (maTextureCoordinateHandle == -1) {
+                throw new RuntimeException("Could not get attrib location for aTextureCoord");
+            }
+
+            uTextureSamplerHandle= GLES20.glGetUniformLocation(mProgramId,"sTexture");
+            OpenGlUtil.checkGlError("glGetUniformLocation uniform sTexture");
+
+            uMVPMatrixHandle=GLES20.glGetUniformLocation(mProgramId,"uMVPMatrix");
+            OpenGlUtil.checkGlError("glGetUniformLocation uMVPMatrix");
+
+
 
         }
 
         @Override
-        public void onSurfaceChanged(GL10 gl10, int i, int i1) {
-
+        public void onSurfaceChanged(GL10 gl10, int surfaceWidth, int surfaceHeight) {
+            GLES20.glViewport(0, 0, surfaceWidth, surfaceHeight);
+            this.surfaceWidth = surfaceWidth;
+            this.surfaceHeight = surfaceHeight;
         }
 
         @Override
         public void onDrawFrame(GL10 gl10) {
 
+            GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);//黑色背景
+            GLES20.glClear( GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+
+            GLES20.glFrontFace(GLES20.GL_CW);
+            GLES20.glCullFace(GLES20.GL_BACK);
+            GLES20.glEnable(GLES20.GL_CULL_FACE);
+
+            GLES20.glViewport(0,0,surfaceWidth,surfaceHeight);
+
+            GLES20.glEnable(GLES20.GL_BLEND);
+            GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+
+            GLES20.glUseProgram(mProgramId);
+            OpenGlUtil.checkGlError("glUseProgram");
+
+
+            if (imageTextureId != 0) {
+                GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+                GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, imageTextureId);
+                GLES20.glUniform1i(uTextureSamplerHandle, 0);
+            }
+
+            mTexCoordinateBuffer.position(0);
+            GLES20.glVertexAttribPointer(maTextureCoordinateHandle, 2, GLES20.GL_FLOAT, false, 0, mTexCoordinateBuffer);
+            OpenGlUtil.checkGlError("glVertexAttribPointer maTextureHandle");
+            GLES20.glEnableVertexAttribArray(maTextureCoordinateHandle);
+            OpenGlUtil.checkGlError("glEnableVertexAttribArray maTextureHandle");
+
+            mVerticesBuffer.position(0);
+            GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false, 0, mVerticesBuffer);
+            OpenGlUtil.checkGlError("glVertexAttribPointer maPosition");
+            GLES20.glEnableVertexAttribArray(maPositionHandle);
+            OpenGlUtil.checkGlError("glEnableVertexAttribArray maPositionHandle");
+
+            Matrix.setIdentityM(projectionMatrix,0);
+            GLES20.glUniformMatrix4fv(uMVPMatrixHandle, 1, false, projectionMatrix, 0);
+
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+
+            GLES20.glDisable(GLES20.GL_BLEND);
+            GLES20.glDisable(GLES20.GL_CULL_FACE);
         }
     }
+
 
 }
---
+++ app/src/main/java/com/xogrp/john/sensor/openGl/OpenGlUtil.java
@@ -0,0 +1,131 @@
+package com.xogrp.john.sensor.openGl;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by john on 13/11/2017.
+ */
+
+public class OpenGlUtil {
+
+    private static final String TAG = "ziq";
+
+    public static int createProgram(String vertexSource, String fragmentSource) {
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
+        if (vertexShader == 0) {
+            return 0;
+        }
+        int pixelShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSource);
+        if (pixelShader == 0) {
+            return 0;
+        }
+
+        int program = GLES20.glCreateProgram();
+        if (program != 0) {
+            GLES20.glAttachShader(program, vertexShader);
+            checkGlError("glAttachShader");
+            GLES20.glAttachShader(program, pixelShader);
+            checkGlError("glAttachShader");
+            GLES20.glLinkProgram(program);
+            int[] linkStatus = new int[1];
+            GLES20.glGetProgramiv(program, GLES20.GL_LINK_STATUS, linkStatus, 0);
+            if (linkStatus[0] != GLES20.GL_TRUE) {
+                Log.e(TAG, "Could not link program: ");
+                Log.e(TAG, GLES20.glGetProgramInfoLog(program));
+                GLES20.glDeleteProgram(program);
+                program = 0;
+            }
+        }
+        return program;
+    }
+
+
+    public static int loadShader(int shaderType, String source) {
+        int shader = GLES20.glCreateShader(shaderType);
+        if (shader != 0) {
+            GLES20.glShaderSource(shader, source);
+            GLES20.glCompileShader(shader);
+            int[] compiled = new int[1];
+            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+            if (compiled[0] == 0) {
+                Log.e(TAG, "Could not compile shader " + shaderType + ":");
+                Log.e(TAG, GLES20.glGetShaderInfoLog(shader));
+                GLES20.glDeleteShader(shader);
+                shader = 0;
+            }
+        }
+        return shader;
+    }
+
+    public static void checkGlError(String label) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, label + ": glError " + error);
+            throw new RuntimeException(label + ": glError " + error);
+        }
+    }
+
+
+
+    public static Bitmap loadBitmapFromAssets(Context context, String filePath){
+        InputStream inputStream = null;
+        try {
+            inputStream = context.getResources().getAssets().open(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(inputStream==null) return null;
+        BitmapFactory.Options options=new BitmapFactory.Options();
+        options.inScaled=false;
+        Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
+        return bitmap;
+    }
+
+
+    public static int getTextureFromBitmap(Bitmap bitmap,int imageSize[]){
+        final int[] textureObjectIds=new int[1];
+        GLES20.glGenTextures(1,textureObjectIds,0);
+        if (textureObjectIds[0]==0){
+            Log.d(TAG,"Failed at glGenTextures");
+            return 0;
+        }
+
+        if (bitmap==null){
+            Log.d(TAG,"Failed at decoding bitmap");
+            GLES20.glDeleteTextures(1,textureObjectIds,0);
+            return 0;
+        }
+
+        if(imageSize!=null && imageSize.length>=2){
+            imageSize[0]=bitmap.getWidth();
+            imageSize[1]=bitmap.getHeight();
+        }
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,textureObjectIds[0]);
+
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+        GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D,0,bitmap,0);
+        bitmap.recycle();
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D,0);
+        return textureObjectIds[0];
+    }
+
+
+
+}
