9a78e2e9e69666ecd437a40ed32966b8c6f7099d
ziq358 <461804160@qq.com>
2018-04-07 11:17:51 +0800

    高斯模糊，， 对bitmap 的修改，会影响已经设置的bitmap

--- app/src/main/java/com/xogrp/john/sensor/JniTestActivity.java
+++ app/src/main/java/com/xogrp/john/sensor/JniTestActivity.java
@@ -2,6 +2,10 @@ package com.xogrp.john.sensor;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -29,7 +33,12 @@ public class JniTestActivity extends AppCompatActivity {
         ivRoot = (ImageView) findViewById(R.id.gaussBlur_root);
         ivResult = (ImageView) findViewById(R.id.gaussBlur_result);
         final Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.checklist_default);
-        ivRoot.setImageBitmap(bitmap);
+        Bitmap copyBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),bitmap.getConfig());//
+        Canvas canvas = new Canvas(copyBitmap);
+        Paint paint = new Paint();
+        Matrix m = new Matrix();
+        canvas.drawBitmap(bitmap, m, paint);//生成了了一个bitmap 的副本，不受之后 原bitmap的变化影响
+        ivRoot.setImageBitmap(copyBitmap);
         ivResult.setImageBitmap(bitmap);
         findViewById(R.id.gaussBlur).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +54,8 @@ public class JniTestActivity extends AppCompatActivity {
         //调用native方法，传入Bitmap对象，对Bitmap进行高斯迷糊处理
         NdkUtil ndkUtil = new NdkUtil();
         ndkUtil.gaussBlur(bitmap);
-        //把Bitmap对象设置给iv2
-        ivResult.setImageBitmap(bitmap);
+        //把Bitmap对象设置给iv2，，因为是直接修改bitmap，之前setImageBitmap 中的imageView 的bitmap 也变了，所以不用再设置
+//        ivResult.setImageBitmap(bitmap);
     }
 
 }
