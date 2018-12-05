af3f5d3903ae70bd9718887bcad6ee37d7700286
wuyanqiang <wuyanqiang>
2018-12-05 18:42:49 +0800

    image loader 分析

--- app/src/main/java/com/xogrp/john/sensor/UniversalImageLoaderAty.java
+++ app/src/main/java/com/xogrp/john/sensor/UniversalImageLoaderAty.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
@@ -19,7 +20,16 @@ public class UniversalImageLoaderAty extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_universal_image_loader);
         ImageView imageView = (ImageView) findViewById(R.id.image);
-        ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(this);
+//        ImageLoaderConfiguration configuration = ImageLoaderConfiguration.createDefault(this);
+
+        DisplayImageOptions.Builder displayOptionsBuilder = new DisplayImageOptions.Builder();
+        displayOptionsBuilder.cacheOnDisk(true);
+        //1M   或 10张
+        ImageLoaderConfiguration.Builder builder = new ImageLoaderConfiguration.Builder(this)
+                .diskCacheSize(1024 * 1024)
+                .diskCacheFileCount(10)
+                .defaultDisplayImageOptions(displayOptionsBuilder.build());
+        ImageLoaderConfiguration configuration = builder.build();
         ImageLoader.getInstance().init(configuration);
         ImageLoader.getInstance().displayImage("http://img3.imgtn.bdimg.com/it/u=496641143,3869583512&fm=200&gp=0.jpg", imageView);
     }
